7c23b4a511c46b01a5304d1a1f66baac9583b2fe
pz10097 <104281878+pz10097@users.noreply.github.com>
2022-12-05 01:04:51 +0100

    Added model

--- Fake news detection.py
+++ Fake news detection.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
-
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
 from tensorflow.keras import layers
 
 df = pd.read_csv('fake_news/news.csv')
 print(df.columns)
+df["label"].replace({"REAL": 1, "FAKE": 0}, inplace=True)
 y = df.iloc[:, -1]
 df["new_text"] = df["title"] + df["text"]
 X = df.iloc[:, -1]
@@ -21,7 +23,7 @@ def length_dicsarding_space(text):
 
 max_features = 10000
 #sequence_length = max(list(map(length_dicsarding_space, X)))
-sequence_length = 40000
+sequence_length = 25000
 print(sequence_length)
 
 vectorize_layer = layers.TextVectorization(
@@ -34,4 +36,41 @@ vectorize_layer.adapt(X)
 print(vectorize_layer(X[0]))
 
 vectorized_text = vectorize_layer(X)
-print(vectorized_text)
+#print(vectorized_text)
+vectorized_text = np.array(vectorized_text)
+print(vectorized_text.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(vectorized_text, y, test_size=.33, random_state=4)
+X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=.33, random_state=42)
+
+print(X_train.shape)
+print(X_val.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_val.shape)
+print(y_test.shape)
+
+embedding_dim = 16
+
+model = tf.keras.Sequential([
+  layers.Embedding(max_features + 1, embedding_dim),
+  layers.Dropout(0.2),
+  layers.GlobalAveragePooling1D(),
+  layers.Dropout(0.2),
+  layers.Dense(1)])
+
+model.compile(loss=tf.losses.BinaryCrossentropy(from_logits=True),
+              optimizer='adam',
+              metrics=tf.metrics.BinaryAccuracy(threshold=0.0))
+
+epochs = 10
+history = model.fit(
+    X_train,
+    y_train,
+    validation_data=(X_val, y_val),
+    epochs=epochs)
+
+loss, accuracy = model.evaluate(X_test, y_test)
+print(loss)
+print(accuracy)
+
